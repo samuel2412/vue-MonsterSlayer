a3cad668f62d32efe9d6c9b4f297580f31019c33
Samuel Alves <samuel241296@hotmail.com>
2020-01-28 22:26:12 -0300

    refatoraçao dos metodos de ataque

--- app.js
+++ app.js
@@ -1,45 +1,59 @@
 new Vue({
-	el: '#app',
-  data:{
-          playerHp: 100,
-          monsterHp: 100,
-          gameIsRunning: false,
+  el: '#app',
+  data: {
+    playerHp: 100,
+    monsterHp: 100,
+    gameIsRunning: false,
+  },
+  methods: {
+    startGame: function () {
+      this.gameIsRunning = true;
+      this.playerHp = 100;
+      this.monsterHp = 100;
     },
-    methods:{
-      startGame: function(){
-        this.gameIsRunning= true;
-        this.playerHp= 100;
-        this.monsterHp= 100;
-      },
-      attack: function(){
-        var max= 10;
-        var min= 3;
-        var damage = Math.max(Math.floor(Math.random() * max) +1, min);
-        this.monsterHp -= damage;
+    attack: function () {
 
-        if(this.monsterHp <= 0){
-          alert('You won!');
-          this.gameIsRunning=false;
-          return;
-        }
-        max = 12;
-        min = 5;
-        damage = Math.max(Math.floor(Math.random() * max) +1, min);
-        this.playerHp -= damage;
+      this.monsterHp -= this.calculateDamage(3, 10);
 
-        if(this.playerHp <= 0){
-          alert('You lost!');
-          this.gameIsRunning=false;
+      if (this.checkWin()) {
+        return;
+      }
+
+      this.playerHp -= this.calculateDamage(5, 12);
+
+      this.checkWin()
+    },
+    specialAttack: function () {
+
+    },
+    heal: function () {
+
+    },
+    givUp: function () {
+
+    },
+    calculateDamage: function (min, max) {
+      return Math.max(Math.floor(Math.random() * max) + 1, min);
+    },
+
+    checkWin: function () {
+      if (this.monsterHp <= 0) {
+        if (confirm('You won! New Game?')) {
+          this.startGame();
+        } else {
+          this.gameIsRunning = false;
         }
-      },
-      specialAttack: function(){
-       
-      },
-      heal: function(){
-       
-      },
-      givUp: function(){
-       
-      },
+        return true;
+      } else if (this.playerHp <= 0) {
+        if (confirm('You lost! New Game?')) {
+          this.startGame();
+        } else {
+          this.gameIsRunning = false;
+        }
+        return true;
+      }
+      return false;
+
     }
+  },
 })
