630e2ef913cfb2a94284527b42095c0eeb54d534
Samuel Alves <samuel241296@hotmail.com>
2020-01-28 22:12:25 -0300

    ataques adicionados, checagem se player ou monstro foram derrotados

--- app.js
+++ app.js
@@ -5,5 +5,41 @@ new Vue({
           monsterHp: 100,
           gameIsRunning: false,
     },
-    
+    methods:{
+      startGame: function(){
+        this.gameIsRunning= true;
+        this.playerHp= 100;
+        this.monsterHp= 100;
+      },
+      attack: function(){
+        var max= 10;
+        var min= 3;
+        var damage = Math.max(Math.floor(Math.random() * max) +1, min);
+        this.monsterHp -= damage;
+
+        if(this.monsterHp <= 0){
+          alert('You won!');
+          this.gameIsRunning=false;
+          return;
+        }
+        max = 12;
+        min = 5;
+        damage = Math.max(Math.floor(Math.random() * max) +1, min);
+        this.playerHp -= damage;
+
+        if(this.playerHp <= 0){
+          alert('You lost!');
+          this.gameIsRunning=false;
+        }
+      },
+      specialAttack: function(){
+       
+      },
+      heal: function(){
+       
+      },
+      givUp: function(){
+       
+      },
+    }
 })
